2f8e701a19bf8955b48517306570d0f7b9e0702d
HyunSoo Park <gustn400@naver.com>
2022-12-19 17:57:22 +0900

    feat: 서치 창에 검색하면 검색한 내용을 찾는 서치 창으로 이동

--- src/Routes/Search.tsx
+++ src/Routes/Search.tsx
@@ -1,10 +1,22 @@
+import { useLocation } from "react-router";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  position: relative;
+  top: 100px;
+  padding: 20px;
+`;
+
+const SearchName = styled.h1``;
+
 function Search() {
+  const location = useLocation();
+  const keyword = new URLSearchParams(location.search).get("keyword");
+
   return (
-    <div style={{ height: "200vh" }}>
-      <div style={{ height: 68 }}></div>
-      <h1>Search</h1>
-    </div>
+    <Wrapper>
+      <SearchName>{keyword}에 대한 검색 결과입니다.</SearchName>
+    </Wrapper>
   );
 }
-
 export default Search;
